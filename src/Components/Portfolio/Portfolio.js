d2c684c572b2d0f33e1501b6b0832954d80d970e
Dennis Macharia <dennismacharia4747@gmail.com>
2019-11-08 16:10:45 +0300

    add cicle and triangle with no styling

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import staff4 from '../../assets/staff-4.png'
 import './portfolio.css'
 
 const Portfolio = () => {
@@ -33,7 +34,24 @@ const Portfolio = () => {
                 
             </div>
             
-            <div className='portfolio-section__bio'></div>
+            {/* Portfolio bio */}
+            <div className='portfolio-section__bio'>
+                <div className='portfolio-section__designer-info'>
+                    <div className='team-section__images--staff-img u-margin-bottom-small'>
+                        <img className='' src={staff4} alt='staff 1' />
+                    </div>
+                    <p className='business__info portfolio-section__info u-center-text u-margin-bottom-medium'> Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua.</p>
+                    <div className='portfolio-section__designer-line u-margin-bottom-medium'></div>
+                    <h1 className='business__info portfolio-section__designer-name to-white u-margin-bottom-small'>Shariah Hossain</h1>
+                    <h5 className='team-section__bio-title to-white'>UI/UX Designer</h5>
+
+                <div className='portfolio-section__circle '>
+                    <div className='arrow left'></div>
+                </div>
+                </div>
+            </div>
 
             
         </div>
